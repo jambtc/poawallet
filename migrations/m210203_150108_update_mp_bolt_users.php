adc8763fd3f178e3df4196b78eff5adcb8b6d906
Sergio Casizzone <sergio.casizzone@agenziaentrate.it>
2021-02-22 13:07:43 +0100

    from bolt to megapay migration

--- migrations/m210203_150108_update_mp_bolt_users.php
+++ migrations/m210203_150108_update_mp_bolt_users.php
@@ -29,8 +29,14 @@ class m210203_150108_update_mp_bolt_users extends Migration
             'picture' => $this->string(255)->defaultValue(NULL),
             'provider' => $this->string(20)->defaultValue(NULL),
             'facade' => $this->string(20)->defaultValue(NULL),
-
         ]);
+
+        $this->execute('INSERT INTO `mp_users`
+          (`id`, `username`, `password`, `ga_secret_key`, `activation_code`, `status_activation_code`,
+            `oauth_provider`, `oauth_uid`, `email`, `provider`, `facade`)
+            SELECT  b.id_user, b.email,b.password,b.ga_secret_key,b.activation_code,b.status_activation_code,
+            b.oauth_provider,b.oauth_uid,b.email,b.oauth_provider, "dashboard"
+            FROM `bolt_users` AS b WHERE 1 ');
 
     }
 
