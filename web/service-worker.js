5e31626be2077e384325a543887655d209c35b4d
Sergio Casizzone <jambtc@gmail.com>
2021-04-26 15:56:55 +0200

    sw update

--- web/service-worker.js
+++ web/service-worker.js
@@ -3,7 +3,7 @@ importScripts('src/js/idb.js');
 importScripts('src/js/idb-utility.js');
 
 var CACHE_STATIC_NAME = 'megapay-static-007';
-var CACHE_DYNAMIC_NAME = 'megapay-dynamic-006';
+var CACHE_DYNAMIC_NAME = 'megapay-dynamic-007';
 
 var OFFLINE_URL = 'offline.html';
 
