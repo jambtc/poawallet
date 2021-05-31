64607aea6c93337ee617d58374c85b7c3c588e8c
Sergio Casizzone <jambtc@gmail.com>
2021-05-31 15:51:17 +0200

    folder rights

--- update.sh
+++ update.sh
@@ -5,6 +5,13 @@ echo Updating...
 git stash
 git pull
 
+### Folder rights
+chgrp www-data web/assets
+chmod g+w web/assets/
+
+chgrp www-data runtime
+chmod g+w runtime/
+
 echo Versioning...
 git rev-parse HEAD>version.txt
 
